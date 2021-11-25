214677aaa6bbe8ffa9f7bc5b2d31966952e88117
YUXIN-Z <zhangyuxin201709@gmail.com>
2021-11-25 14:03:23 -0800

    easyocr to read image

--- image_reader.py
+++ image_reader.py
@@ -1,1 +1,15 @@
-# empty file
+# read image
+from PIL import Image
+# csv
+import csv
+
+# scan image
+import easyocr
+# im = Image.open('Albuna pyramidalis coloradensis female (1dorsal).jpg')
+# print(im)
+reader = easyocr.Reader(['en'])
+result = reader.readtext('Albuna pyramidalis coloradensis female (1dorsal).jpg')
+print(result[1][0])
+
+workbook = xlsxwriter.Workbook('image.xlsx')
+worksheet = workbook.add_worksheet()
