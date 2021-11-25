f9ff62d5f90cd783908279117d428aee0a704543
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-11-25 14:21:02 -0800

    clean up

--- image_reader.py
+++ image_reader.py
@@ -5,8 +5,7 @@ import csv
 
 # scan image
 import easyocr
-# im = Image.open('Albuna pyramidalis coloradensis female (1dorsal).jpg')
-# print(im)
+
 reader = easyocr.Reader(['en'])
 result = reader.readtext(
     'example_images/Albuna pyramidalis coloradensis female (1dorsal).jpg')
