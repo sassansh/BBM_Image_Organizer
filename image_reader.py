9c78732be21a6b5ae68d4cd3ef15d1f72f2e0709
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-11-25 15:11:39 -0800

    add all images

--- image_reader.py
+++ image_reader.py
@@ -10,3 +10,19 @@ reader = easyocr.Reader(['en'])
 result = reader.readtext(
     'example_images/Albuna pyramidalis coloradensis female (1dorsal).jpg')
 print(result)
+
+result = reader.readtext(
+    'example_images/Climaciella brunnea (1dorsal).jpg')
+print(result)
+
+result = reader.readtext(
+    'example_images/Climaciella brunnea (2ventral).jpg')
+print(result)
+
+result = reader.readtext(
+    'example_images/Climaciella brunnea (3lateral).jpg')
+print(result)
+
+result = reader.readtext(
+    'example_images/Leptomantispa pulchella (3lateral).jpg')
+print(result)
