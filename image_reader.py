f77c47d416f21bfc8deb9797b088ccc9a95a1388
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-12-07 16:04:27 -0800

    improved algo

--- image_reader.py
+++ image_reader.py
@@ -25,7 +25,11 @@ with open('results ' + date_time + '.csv', 'a', encoding='UTF8') as f:
 directory = 'example_images'
 for imageName in os.listdir(directory):
 
-    print("========== STARTING: " + imageName + " ==========")
+    now_image = datetime.now()
+    date_time_image = now_image.strftime("%H:%M:%S")
+
+    print("========== STARTING: " + imageName +
+          " ========== " + date_time_image)
 
     # Skip non-image files
     if not imageName.endswith(".jpg") and not imageName.endswith(".png") and not imageName.endswith(".jpeg"):
@@ -43,7 +47,7 @@ for imageName in os.listdir(directory):
     # Crop image
     height, width, channels = image.shape
     left = 0
-    top = int(height * 5.1 / 6)
+    top = int(height * 4.5 / 6)
     right = width
     bottom = int(height * 5.8 / 6)
     image = image[top:bottom, left:right]
@@ -101,25 +105,78 @@ for imageName in os.listdir(directory):
     else:
         print("Picking Red OCR")
 
+    # empty all variables
+    scientific_name_image = ''
+    scientific_name_image_conf = ''
+    SEM_number_image = 'OCR FAILED'
+    SEM_number_image_conf = '0'
+    angle_image = ''
+    angle_image_conf = ''
+    scientific_name_filename = ''
+    SEM_number_filename = ''
+    angle_filename = ''
+    scientific_name_matches = ''
+    SEM_number_matches = ''
+    angle_matches = ''
+    all_matches = ''
+
     # Prepare data from image
     filename = imageName
 
     try:
-        scientific_name_image = result[0][1].split(' (')[0]
+        scientific_name_image = result[0][1].split('(')[0].rstrip()
         scientific_name_image_conf = result[0][2]
-        SEM_number_image = 'SEM' + result[1][1].upper().split('SEM')[1]
-        SEM_number_image_conf = result[1][2]
-        angle_image = (result[0][1].split(' (')[1]).split(')')[0]
+        angle_image = (result[0][1].split('(')[1]).split(')')[0]
         angle_image_conf = result[0][2]
     except Exception as e:
+        print("ERROR:")
         print(e)
-        print("OCR failed for " + imageName)
-        scientific_name_image = 'OCR FAILED'
-        scientific_name_image_conf = '0'
+        print("OCR failed for " + imageName +
+              "'s name or angle... trying combining array")
+        try:
+            combine_name_angle = ''
+            combine_name_angle_conf = 0
+            i = 0
+            while i < len(result):
+                if ('SEM' not in result[i][1]):
+                    combine_name_angle += result[i][1] + ' '
+                    combine_name_angle_conf += result[i][2]
+                i += 1
+            combine_name_angle_conf = combine_name_angle_conf / i
+            scientific_name_image = combine_name_angle.split('(')[0].rstrip()
+            scientific_name_image_conf = combine_name_angle_conf
+            angle_image = (combine_name_angle.split('(')[1]).split(')')[0]
+            angle_image_conf = combine_name_angle_conf
+        except Exception as e:
+            print("ERROR:")
+            print(e)
+            print("OCR failed for" + imageName + "'s name or angle")
+            scientific_name_image = 'OCR FAILED'
+            scientific_name_image_conf = '0'
+            angle_image = 'OCR FAILED'
+            angle_image_conf = '0'
+
+    # Convert capital I to l
+    if ("I" in scientific_name_image[1:] and scientific_name_image != 'OCR FAILED'):
+        print("Found I in " + imageName + "'s OCR name")
+        scientific_name_image = scientific_name_image[0] + \
+            scientific_name_image[1:].replace("I", "l")
+
+    # Convert € to C
+    scientific_name_image = scientific_name_image.replace("€", "C")
+
+    try:
+        for row in result:
+            if 'SEM' in row[1]:
+                SEM_number_image = 'SEM' + row[1].upper().split('SEM')[1]
+                SEM_number_image_conf = row[2]
+                break
+    except Exception as e:
+        print("ERROR:")
+        print(e)
+        print("OCR failed for " + imageName + "'s SEM_number")
         SEM_number_image = 'OCR FAILED'
         SEM_number_image_conf = '0'
-        angle_image = 'OCR FAILED'
-        angle_image_conf = '0'
 
     # Prepare data from filename
 
@@ -134,12 +191,19 @@ for imageName in os.listdir(directory):
         angle_filename = ''.join(
             i for i in raw_angle_filename if not i.isdigit())
     except Exception as e:
+        print("ERROR:")
         print(e)
         print("Filename parsing failed for " + imageName)
         scientific_name_filename = 'PARSING FAILED'
         SEM_number_filename = 'PARSING FAILED'
         raw_angle_filename = 'PARSING FAILED'
         angle_filename = 'PARSING FAILED'
+
+    # Determine if extra characters are present in OCR name
+    if scientific_name_filename in scientific_name_image:
+        print("Scientific name in OCRed name, replacing OCR name(" +
+              scientific_name_image + ") with filename")
+        scientific_name_image = scientific_name_filename
 
     # Matching analysis
 
@@ -171,4 +235,9 @@ for imageName in os.listdir(directory):
 
         # write the data
         writer.writerow(data)
-    print("========== FINISHED: " + imageName + " ==========\n\n")
+    finish_image = datetime.now()
+    date_time_image_finish = finish_image.strftime("%H:%M:%S")
+    elapsed = finish_image - now_image
+    print("Time to complete OCR: " + str(elapsed))
+    print("========== FINISHED: " + imageName +
+          " ========== " + date_time_image_finish + "\n\n")
