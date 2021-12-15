1ace08292fb2a3e7b0df0dac7eeff4fc9b4bd336
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-12-15 14:06:04 -0800

    refactor

--- image_reader_v2.py
+++ image_reader_v2.py
@@ -34,31 +34,164 @@ def read_image(image, top, bottom, left, right, filter, reader):
     result = reader.readtext(byte_im)
     return result
 
+def write_csv(filename, data):
+    """
+    Write data/header into the csv file
+    """
+    with open(filename, 'a', encoding='UTF8') as f:
+        writer = csv.writer(f)
 
-# def parse_filename(imageName):
+        # write the header
+        writer.writerow(data)
+
+    
+
+def parse_filename(filename):
     """
     Read "scientific_name_filename,SEM_number_filename,angle_filename" from the given file
     """
+     # Prepare data from filename
+    scientific_name = ""
+    SEM_number = ""
+    raw_angle = ""
+    angle = ""
+
+    try:
+        scientific_name = filename.split(' (')[0]
+        SEM_number = ''
+        if 'SEM' in filename:
+            SEM_number = 'SEM' + \
+                filename.split('SEM')[1].split('.')[0]
+        raw_angle = filename.split(' (')[1].split(')')[0]
+        angle = ''.join(
+            i for i in raw_angle if not i.isdigit())
+
+        if SEM_number == '':
+            x = re.search("[A-Z]{2,}-[0-9]{3,}", filename)
+            try:
+                SEM_number = "SEM-UBC " + x.group()
+            except:
+                SEM_number = ""
+
+    except Exception as e:
+        print("ERROR:")
+        print(e)
+        print("Filename parsing failed for " + imagePath)
+        scientific_name = 'PARSING FAILED'
+        SEM_number = 'PARSING FAILED'
+        angle = 'PARSING FAILED'
+    
+    return scientific_name, SEM_number, angle
+
+def find_SEM_conf(result):
+    """
+    Find the confidence of the SEM number
+    """
+    conf = 0
+    
+    for row in result:
+        if 'SEM' in row[1].upper():
+            conf = row[2]
+            break
+        
+    return conf
+
+def SEM_exists(result):
+    """
+    Return True if the result contains a SEM number
+    """
+    exists = False
+    
+    for row in result:
+        if 'SEM' in row[1].upper():
+            exists = True
+            break
+        
+    return exists
+
+def cropped_scan(image, filter, reader):
+    """
+    Perform OCR on bottom right corner of image using original filter
+    """
+    # Set crop dimensions
+    height, width, channels = image.shape
+    left = int(width * 3 / 5)
+    top = int(height * 4.5 / 6)
+    right = width
+    bottom = height
+    
+    # Perform OCR
+    result = read_image(image, top, bottom, left, right, filter, reader)
+
+    # Get SEM confidence
+    conf = find_SEM_conf(result)
+    
+    return result, conf
+    
+def full_scan(image, filter, reader):
+    """
+    Perform OCR on the whole image using original filter
+    """
+    # Set crop dimensions
+    height, width, channels = image.shape
+    left = 0
+    top = 0
+    right = width
+    bottom = height
+    
+    # Perform OCR
+    result = read_image(image, top, bottom, left, right, filter, reader)
+
+    # Get SEM confidence
+    conf = find_SEM_conf(result)
+    
+    return result, conf
+    
+def parse_result(result):
+    """
+    Parse the SEM number from the selected result 
+    and put 'OCR FAILED' if it causes error.
+    """
+    try:
+        for row in result:
+            if 'SEM' in row[1].upper():
+                SEM_number = 'SEM' + row[1].upper().split('SEM')[1]
+                break
+    except Exception as e:
+        print("ERROR:")
+        print(e)
+        print("OCR failed for " + imagePath + "'s SEM_number")
+        SEM_number = 'OCR FAILED'
+    
+    return SEM_number
+    
 
 
-# new main body
-# set up the reader
+# Main Body
+# Global Static Variables
+MIN_CONFIDENCE = 0.65
+IMAGES_ROOT_DIRECTORY = 'example_images/'
+CSV_HEADER = ['file_path', 'SEM_number_image', 'SEM_number_image_conf',
+              'scientific_name_filename', 'SEM_number_filename', 'angle_filename']
+
+# Initialize time & OCR Reader
 now = datetime.now()
 date_time = now.strftime("%m-%d-%Y %H.%M.%S")
 reader = easyocr.Reader(['en'])
 
-with open('results ' + date_time + '.csv', 'a', encoding='UTF8') as f:
-    header = ['file_path', 'SEM_number_image', 'SEM_number_image_conf',
-              'scientific_name_filename', 'SEM_number_filename', 'angle_filename']
+# Create results CSV file with header
+results_csv_filename = "results_" + date_time + ".csv"
+write_csv(results_csv_filename, CSV_HEADER)
 
-    writer = csv.writer(f)
+# Read all images in the directory 
+for imagePath in glob.iglob(IMAGES_ROOT_DIRECTORY + '**/*', recursive=True):
+    #Initialize variables
+    sem_conf_red = 0
+    sem_conf_full = 0
+    scientific_name_filename = ""
+    SEM_number_filename = ""
+    angle_filename = ""
 
-    # write the header
-    writer.writerow(header)
-
-
-directory = 'example_images/'
-for imagePath in glob.iglob(directory + '**/*', recursive=True):
     # Extract filename
     imageFileName = os.path.basename(imagePath)
 
@@ -72,40 +205,10 @@ for imagePath in glob.iglob(directory + '**/*', recursive=True):
     print("\n========== STARTING: " + imagePath +
         " ========== " + date_time_image)
 
-    # Prepare data from filename
-    scientific_name_filename = ""
-    SEM_number_filename = ""
-    raw_angle_filename = ""
-    angle_filename = ""
+    # Parse filename
+    scientific_name_filename, SEM_number_filename, angle_filename = parse_filename(imageFileName)
 
-
-
-    try:
-        scientific_name_filename = imageFileName.split(' (')[0]
-        SEM_number_filename = ''
-        if 'SEM' in imageFileName:
-            SEM_number_filename = 'SEM' + \
-                imageFileName.split('SEM')[1].split('.')[0]
-        raw_angle_filename = imageFileName.split(' (')[1].split(')')[0]
-        angle_filename = ''.join(
-            i for i in raw_angle_filename if not i.isdigit())
-
-        if SEM_number_filename == '':
-            x = re.search("[A-Z]{2,}-[0-9]{3,}", imageFileName)
-            try:
-                SEM_number_filename = "SEM-UBC " + x.group()
-                print("SEM Filename using REGEX: " + SEM_number_filename)
-            except:
-                SEM_number_filename = ""
-
-    except Exception as e:
-        print("ERROR:")
-        print(e)
-        print("Filename parsing failed for " + imagePath)
-        scientific_name_filename = 'PARSING FAILED'
-        SEM_number_filename = 'PARSING FAILED'
-        angle_filename = 'PARSING FAILED'
-
+    # Perform OCR if filename doesn't contain the SEM number
     if SEM_number_filename == "" or SEM_number_filename == 'PARSING FAILED':
         # Open Image
         image = cv2.imread(
@@ -115,134 +218,78 @@ for imagePath in glob.iglob(directory + '**/*', recursive=True):
         if not hasattr(image, 'shape'):
             continue
 
-        # Try cropped image first
-        height, width, channels = image.shape
-        left = int(width * 3 / 5)
-        top = int(height * 4.5 / 6)
-        right = width
-        bottom = height
-
-        result_original = read_image(
-            image, top, bottom, left, right, "none", reader)
-
-        try:
-            sem_conf_orignal = result_original[0][2]
-        except:
-            sem_conf_orignal = 0
+        # Perform cropped original OCR
+        result_original, sem_conf_orignal = cropped_scan(image, "original", reader)
 
         result = result_original
-        original_better = True
         print("ORIGINAL OCR:")
         print(result_original)
 
-        # Try cropped image with red filter
-        if (sem_conf_orignal < 0.65):
+        # If low confidence, try red filter
+        if (sem_conf_orignal < MIN_CONFIDENCE):
             print('Poor detection, trying red channel')
-            result_red = read_image(
-                image, top, bottom, left, right, "red", reader)
+            # Perform cropped red OCR
+            result_red, sem_conf_red = cropped_scan(image, "red", reader)
 
-            try:
-                sem_conf_red = result_red[0][2]
-                print("RED OCR:")
-                print(result_red)
-            except:
-                sem_conf_red = 0
-
-            if (sem_conf_red > sem_conf_orignal):
-                result = result_red
-                original_better = False
-
-        if (original_better):
-            print("Picking Original OCR")
-        else:
+            print("RED OCR:")
+            print(result_red)
+          
+        # Compare original and red OCR results
+        if (sem_conf_red > sem_conf_orignal):
             print("Picking Red OCR")
+            result = result_red
+        else:
+            print("Picking Original OCR")
+     
 
         # See if SEM number is in the cropped OCR
-        SEM_exists = False
-        for row in result:
-            if 'SEM' in row[1].upper():
-                SEM_exists = True
-                print("SEM number detected in cropped OCR")
-                break
+        SEM_exists_in_result = SEM_exists(result)
 
-        if (not SEM_exists):
+        # If SEM number is not in the cropped OCR, perform full OCR
+        if (not SEM_exists_in_result):
             print("SEM number not detected in cropped OCR")
             print("Reading entire image dimentions...")
             # Try non-cropped original image
-            left = 0
-            top = 0
-            right = width
-            bottom = height
-
-            result_original = read_image(
-                image, top, bottom, left, right, "none", reader)
-
-            try:
-                sem_conf_orignal = result_original[0][2]
-            except:
-                sem_conf_orignal = 0
+            result_original, sem_conf_original = full_scan(image, "original", reader)
 
             result = result_original
             original_better = True
             print("WHOLE IMAGE (NO FILTER) OCR:")
             print(result_original)
 
-            # Try cropped image with red filter
-            if (sem_conf_orignal < 0.65):
+            # If low confidence, try red filter
+            if (sem_conf_orignal < MIN_CONFIDENCE):
                 print('Poor detection, trying red channel')
-                result_red = read_image(
-                    image, top, bottom, left, right, "red", reader)
+                result_red, sem_conf_red = full_scan(image, "red", reader)
 
-                try:
-                    sem_conf_red = result_red[0][2]
-                    print("WHOLE IMAGE (RED FILTER) OCR:")
-                    print(result_red)
-                except:
-                    sem_conf_red = 0
+                print("WHOLE IMAGE (RED FILTER) OCR:")
+                print(result_red)
 
-                if (sem_conf_red > sem_conf_orignal):
-                    result = result_red
-                    original_better = False
-
-            if (original_better):
-                print("Picking Original OCR")
-            else:
+            if (sem_conf_red > sem_conf_orignal):
                 print("Picking Red OCR")
+                result = result_red
+            else:
+                print("Picking Original OCR")
 
         # empty all variables
         SEM_number_image = 'OCR FAILED'
         SEM_number_image_conf = '0'
 
-        # Prepare data from image
-        filename = imagePath
+        # Parse SEM number
+        SEM_number_image_conf = find_SEM_conf(result)
+        SEM_number_image = parse_result(result)
 
-        try:
-            for row in result:
-                if 'SEM' in row[1].upper():
-                    SEM_number_image = 'SEM' + row[1].upper().split('SEM')[1]
-                    SEM_number_image_conf = row[2]
-                    break
-        except Exception as e:
-            print("ERROR:")
-            print(e)
-            print("OCR failed for " + imagePath + "'s SEM_number")
-            SEM_number_image = 'OCR FAILED'
-            SEM_number_image_conf = '0'
     else:
         print("Skipping OCR, SEM number detected in filename")
         SEM_number_image = SEM_number_filename
         SEM_number_image_conf = "FROM FILENAME"
-
+    
     # Writing CSV
-
     data = [imagePath, SEM_number_image, SEM_number_image_conf,
             scientific_name_filename, SEM_number_filename, angle_filename]
+    write_csv(results_csv_filename, data)
 
-    with open('results ' + date_time + '.csv', 'a', encoding='UTF8') as f:
-        writer = csv.writer(f)
 
-        # write the data
-        writer.writerow(data)
     finish_image = datetime.now()
     date_time_image_finish = finish_image.strftime("%H:%M:%S")
     elapsed = finish_image - now_image
