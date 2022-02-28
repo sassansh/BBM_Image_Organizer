09a6a8b98ce1e66f241677d313031fe2821c430e
Sassan Shokoohi <sassan_shokoohi@me.com>
2022-02-28 13:03:04 -0800

    Update image_copier.py

--- image_copier.py
+++ image_copier.py
@@ -44,7 +44,7 @@ def parse_filename(filename):
 
 # Main Body
 # Global Static Variables
-IMAGES_ROOT_DIRECTORY = "images/"
+IMAGES_ROOT_DIRECTORY = "website/Main"
 
 
 # Initialize time & OCR Reader
@@ -119,7 +119,7 @@ for imagePath in glob.iglob(IMAGES_ROOT_DIRECTORY + "**/*", recursive=True):
     # Copy the image to a new folder
 
     original = imagePath
-    target = "images_only/" + imagePath.split("images/")[1]
+    target = "website/organism_images" + imagePath.split("website/Main/")[1]
     os.makedirs(os.path.dirname(target), exist_ok=True)
     shutil.copy(original, target)
 
